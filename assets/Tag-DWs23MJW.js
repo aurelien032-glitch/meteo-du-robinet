144eef5680308a2188ba8878d4683c13c0c2f53d
aurelien032-glitch <231720452+aurelien032-glitch@users.noreply.github.com>
2026-09-24 14:59:29 +0200

    Updates

--- assets/Tag-DWs23MJW.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-CZSB0t7I.js";import{V as t}from"./Voyant-Qd_8IV7k.js";function o({ton:s,children:r}){return a.jsxs("span",{className:`tag tone-${s??"neutre"}`,children:[s&&a.jsx(t,{ton:s,taille:14}),r]})}export{o as T};
